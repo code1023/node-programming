7118d5ab41bcedb635ce2db008f1f7b4410513c0
code1023 <znu3173@gmail.com>
2021-04-06 14:33:45 +0900

    routing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // express로 웹 서버 띄워보기
 
 const express = require('express');
+const admin = require('./routes/admin')
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('express start');
 });
+
+// Routing
+app.use('/admin', admin);
 
 app.listen(port, () => {
     console.log('Express listening on port', port);
