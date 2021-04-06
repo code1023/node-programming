b75ea799d6890ee901db8bc54e2f05393a18c65a
code1023 <znu3173@gmail.com>
2021-04-06 14:56:32 +0900

    middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // express로 웹 서버 띄워보기
 
 const express = require('express');
-const admin = require('./routes/admin');
 const nunjucks = require('nunjucks');
+const logger = require('morgan');
+
+const admin = require('./routes/admin');
 
 const app = express();
 const port = 3000;
@@ -11,6 +13,9 @@ nunjucks.configure('template', {
     autoescape: true,
     express: app
 });
+
+// 미들웨어 세팅
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
     res.send('express start');
