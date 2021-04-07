ef77a04c8053a7413394c7ff9a7acccfa5bc34f3
code1023 <znu3173@gmail.com>
2021-04-07 23:17:10 +0900

    db 입력 / 조회

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,7 +1,12 @@
+const models = require('../../models');
+
 exports.get_products = (_, res) => {
-    res.render('admin/products.html',
-        { message: "hello" } // message 란 변수를 템플릿으로 내보낸다.
-    );
+    models.Products.findAll({
+
+    }).then((products) => {
+        // DB에서 받은 products를 products변수명으로 내보냄
+        res.render('admin/products.html', { products: products });
+    });
 }
 
 exports.get_products_write = (_, res) => {
@@ -9,5 +14,12 @@ exports.get_products_write = (_, res) => {
 }
 
 exports.post_products_write = (req, res) => {
-    res.send(req.body);
+    // res.send(req.body);
+    models.Products.create({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description
+    }).then(() => {
+        res.redirect('/admin/products');
+    });
 }
